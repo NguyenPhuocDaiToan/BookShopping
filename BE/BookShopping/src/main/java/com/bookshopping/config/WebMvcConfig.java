0dadc056ce1121b2aab2eba68b27cf4bc048afde
NguyenPhuocDaiToan <daitoan2000@gmail.com>
2024-05-03 01:46:11 +0700

    update

--- BE/BookShopping/src/main/java/com/bookshopping/config/WebMvcConfig.java
+++ BE/BookShopping/src/main/java/com/bookshopping/config/WebMvcConfig.java
@@ -11,12 +11,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
     private final long MAX_AGE_SECS = 3600;
 
     @Value("${app.cors.allowedOrigins}")
-    private String[] allowedOrigins;
+    private String allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(allowedOrigins)
+                .allowedOriginPatterns(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
--- BE/BookShopping/src/main/java/com/bookshopping/controller/HomeController.java
+++ BE/BookShopping/src/main/java/com/bookshopping/controller/HomeController.java
@@ -1,2 +1,18 @@
-package com.bookshopping.controller;public class HomeController {
+package com.bookshopping.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping("")
+    public ResponseEntity<String> getAllCategories() {
+        return new ResponseEntity<>("Xin chao cac ban", HttpStatus.OK);
+    }
 }
