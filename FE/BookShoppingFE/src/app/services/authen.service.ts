7833b893c8b1415c7ab2ae494a9e3fee4e309d13
NguyenPhuocDaiToan <daitoan2000@gmail.com>
2024-05-11 09:18:38 +0700

    update

--- FE/BookShoppingFE/src/app/services/authen.service.ts
+++ FE/BookShoppingFE/src/app/services/authen.service.ts
@@ -1,23 +1,24 @@
-import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {LoginRequest} from '../models/login-request';
-import {RegisterRequest} from '../models/register-request';
-import {Observable} from 'rxjs';
-import {ResponseMessage} from '../models/response-message';
-const API_URL = 'http://localhost:8080/auth';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { LoginRequest } from "../models/login-request";
+import { RegisterRequest } from "../models/register-request";
+import { Observable } from "rxjs";
+import { ResponseMessage } from "../models/response-message";
+const API_URL = "https://bookrecommendation.website/auth";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthenService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   login(loginRequest: LoginRequest): Observable<ResponseMessage> {
-    return this.http.post<ResponseMessage>(API_URL + '/login', loginRequest);
+    return this.http.post<ResponseMessage>(API_URL + "/login", loginRequest);
   }
 
   register(registerRequest: RegisterRequest): Observable<ResponseMessage> {
-    return this.http.post<ResponseMessage>(API_URL + '/register', registerRequest);
-
+    return this.http.post<ResponseMessage>(
+      API_URL + "/register",
+      registerRequest
+    );
   }
 }
--- FE/BookShoppingFE/src/app/user/service/order.service.ts
+++ FE/BookShoppingFE/src/app/user/service/order.service.ts
@@ -1,26 +1,36 @@
-import { Injectable } from '@angular/core';
-import {Observable} from 'rxjs';
-import {ResponseMessage} from '../../models/response-message';
-import {OrderItem} from '../../models/order-item';
-import {HttpClient} from '@angular/common/http';
-import {OrderDetail} from '../../models/order-detail';
-const API_URL = 'http://localhost:8080/api/order';
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { ResponseMessage } from "../../models/response-message";
+import { OrderItem } from "../../models/order-item";
+import { HttpClient } from "@angular/common/http";
+import { OrderDetail } from "../../models/order-detail";
+const API_URL = "https://bookrecommendation.website/api/order";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class OrderService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  order(userId: number, totalPrice: number, orderItems: OrderItem[]): Observable<OrderDetail> {
-    return this.http.post<OrderDetail>(API_URL + '?userId=' + userId + '&&totalPrice=' + totalPrice, orderItems);
+  order(
+    userId: number,
+    totalPrice: number,
+    orderItems: OrderItem[]
+  ): Observable<OrderDetail> {
+    return this.http.post<OrderDetail>(
+      API_URL + "?userId=" + userId + "&&totalPrice=" + totalPrice,
+      orderItems
+    );
   }
 
   findById(orderDetailId: number): Observable<OrderDetail> {
-    return this.http.get<OrderDetail>(API_URL + '/findById?id=' + orderDetailId);
+    return this.http.get<OrderDetail>(
+      API_URL + "/findById?id=" + orderDetailId
+    );
   }
 
   findByUserId(userId: number): Observable<OrderDetail[]> {
-    return this.http.get<OrderDetail[]>(API_URL + '/findByUserId?userId=' + userId);
+    return this.http.get<OrderDetail[]>(
+      API_URL + "/findByUserId?userId=" + userId
+    );
   }
 }
