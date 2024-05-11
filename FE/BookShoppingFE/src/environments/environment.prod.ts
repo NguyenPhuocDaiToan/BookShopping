b959286ed3d051a2a0b2acf03ab2604e4baeb0a5
NguyenPhuocDaiToan <daitoan2000@gmail.com>
2024-05-11 08:12:41 +0700

    update

--- FE/BookShoppingFE/src/environments/environment.prod.ts
+++ FE/BookShoppingFE/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url: "http://localhost:8081/api",
+  url: "https://bookrecommendation.website/api",
   clientId:
     "AXPkXKpXxzFnLRNZHVAvewN-dvyt3zjRVr-bzveAEZz5aOQJWj6A7fxiUSr-kcXpvvrbW94m2ZufGl5L",
 };
