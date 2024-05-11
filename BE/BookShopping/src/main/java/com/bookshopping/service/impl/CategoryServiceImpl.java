98b6fbda763546d7e44205ef2253324df1a5c331
NguyenPhuocDaiToan <daitoan2000@gmail.com>
2024-05-11 07:04:32 +0700

    update

--- BE/BookShopping/src/main/java/com/bookshopping/service/impl/CategoryServiceImpl.java
+++ BE/BookShopping/src/main/java/com/bookshopping/service/impl/CategoryServiceImpl.java
@@ -16,4 +16,11 @@ public class CategoryServiceImpl implements CategoryService {
     public List<Category> findAll() {
         return categoryRepository.findAll();
     }
+
+    @Override
+    public Category save(Category category) {
+        return categoryRepository.save(category);
+    }
+
+
 }
